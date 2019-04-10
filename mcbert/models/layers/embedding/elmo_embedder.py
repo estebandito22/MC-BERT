f22594d9a2bbb9c2410ce55d2e7ef7d77b2de246
Isaac Haberman <isaacjhaberman@gmail.com>
2019-04-10 18:42:42 -0400

    Added elmo_embedder

---
+++ mcbert/models/layers/embedding/elmo_embedder.py
@@ -0,0 +1,29 @@
+
+import sys
+#NEED TO SET FOR LOCAL DIRECTORY
+sys.path.append("/home/ijh216/allennlp")
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from allennlp.modules.elmo import Elmo
+
+#CURRENTLY HARD-CODED
+options_file = "/beegfs/ijh216/elmo/options.json"
+weights_file = "/beegfs/ijh216/elmo/weights.hdf5"
+
+class ElmoEmbedder(torch.nn.Module):
+    def __init__(self, files, emb_dim):
+        super(ElmoEmbedder, self).__init__()
+        
+        self.elmo = Elmo(options_file, weights_file, 1, requires_grad=True) 
+        
+        if torch.cuda.is_available():
+            self.elmo = self.elmo.cuda()
+            
+    def forward(self, input_ids):
+        
+        embds = self.elmo(input_ids)['elmo_representations'][0]
+        
+        return embds
